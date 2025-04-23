2608b159cf2091ba987f8a6c09b6920a164ca151
andrewhoncharenko <goncharenkoandrew5643@gmail.com>
2025-04-23 21:47:03 +0300

    show selected category

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,10 +2,27 @@ import "package:flutter/material.dart";
 
 import "package:meals/data/dummy_data.dart";
 import "package:meals/models/category.dart";
+import "package:meals/models/meal.dart";
+import "package:meals/screens/meals.dart";
 import "package:meals/widgets/category_grid_item.dart";
+import "package:meals/models/category.dart";
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final List<Meal> filteredMeals =
+        dummyMeals
+            .where((meal) => meal.categories.contains(category.id))
+            .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder:
+            (ctx) => MealsScreen(title: category.title, meals: filteredMeals),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +38,10 @@ class CategoriesScreen extends StatelessWidget {
         ),
         children: [
           for (final Category category in availableCategories)
-            CategoryGridItem(category: category),
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () => {_selectCategory(context, category)},
+            ),
         ],
       ),
     );
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -1,30 +1,43 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
-import 'package:meals/models/category.dart';
+import "package:meals/models/category.dart";
 
 class CategoryGridItem extends StatelessWidget {
   final Category category;
+  final void Function() onSelectCategory;
 
-  const CategoryGridItem({super.key, required this.category});
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withValues(alpha: 0.55),
-            category.color.withValues(alpha: 0.9),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () {
+        onSelectCategory();
+      },
+      borderRadius: BorderRadius.circular(16),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withValues(alpha: 0.55),
+              category.color.withValues(alpha: 0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-          color: Theme.of(context).colorScheme.onSurface,
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
         ),
       ),
     );
