2c5ddc272855f5d431a900a10fc9d5fbeae184e3
andrewhoncharenko <goncharenkoandrew5643@gmail.com>
2025-05-01 18:41:15 +0300

    side drawer

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import "package:meals/screens/categories.dart";
 import "package:meals/screens/meals.dart";
 import "package:meals/models/meal.dart";
+import "package:meals/widgets/main_drawer.dart";
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -50,13 +51,14 @@ class TabsScreenState extends State<TabsScreen> {
 
     if (_selectedPageIndex == 1) {
       activePage = MealsScreen(
-        meals: [],
+        meals: _favoriteMeals,
         onToggleFavorite: _toggleMealFavoriteStatus,
       );
       activePageTitle = "Your favorites";
     }
     return Scaffold(
       appBar: AppBar(title: Text(activePageTitle)),
+      drawer: MainDrawer(),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
